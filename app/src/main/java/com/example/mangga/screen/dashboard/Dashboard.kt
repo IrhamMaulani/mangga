ba77ecf900d231ed69ed64c3d0a0284fa3d053a7
Irham <irhammaulani@gmail.com>
2020-03-31 21:55:22 +0800

    add dashboard component

---
+++ app/src/main/java/com/example/mangga/screen/dashboard/Dashboard.kt
@@ -0,0 +1,33 @@
+package com.example.mangga.screen.dashboard
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.mangga.R
+
+class Dashboard : Fragment() {
+
+    companion object {
+        fun newInstance() = Dashboard()
+    }
+
+    private lateinit var viewModel: DashboardViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.dashboard_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(DashboardViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/mangga/screen/dashboard/DashboardViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.mangga.screen.dashboard
+
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
