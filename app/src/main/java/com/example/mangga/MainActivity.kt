3862568028406f98f26b7510e31b65daa8690317
Irham <irhammaulani@gmail.com>
2020-03-31 22:14:19 +0800

    bind bottom nav

--- app/src/main/java/com/example/mangga/MainActivity.kt
+++ app/src/main/java/com/example/mangga/MainActivity.kt
@@ -2,11 +2,29 @@ package com.example.mangga
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.mangga.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val navController = this.findNavController(R.id.myNavHostFragment)
+
+        val appBarConfiguration = AppBarConfiguration.Builder(
+            R.id.dashboardFragment, R.id.mangaListFragment, R.id.profileFragment
+        ).build()
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        binding.navView.setupWithNavController(navController)
     }
 }
