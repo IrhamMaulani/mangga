304969da4a35436789e9a602a16f9a2bb657298b
Irham <irhammaulani@gmail.com>
2020-04-06 02:52:10 +0800

    add glide and notifyDataChanged when set new data

--- app/src/main/java/com/example/mangga/screen/dashboard/TopMangaAdapter.kt
+++ app/src/main/java/com/example/mangga/screen/dashboard/TopMangaAdapter.kt
@@ -2,11 +2,23 @@ package com.example.mangga.screen.dashboard
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.net.toUri
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.mangga.R
 import com.example.mangga.databinding.TopMangaItemBinding
 import com.example.mangga.model.Manga
 
-class TopMangaAdapter (private val listManga : ArrayList<Manga>) : RecyclerView.Adapter<TopMangaAdapter.ListViewHolder>() {
+class TopMangaAdapter () : RecyclerView.Adapter<TopMangaAdapter.ListViewHolder>() {
+
+     var listManga =   listOf<Manga>()
+            set(value){
+            field = value
+                notifyDataSetChanged()
+    }
+
+
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
@@ -26,6 +38,14 @@ class TopMangaAdapter (private val listManga : ArrayList<Manga>) : RecyclerView.
             itemBinding.tvMangaTitle.text = manga.title
             itemBinding.tvMangaReleaseDate.text = manga.startDate
             itemBinding.tvMangaScore.text = manga.score
+
+            Glide.with(itemBinding.ivMangaCover.context)
+                .load(manga.imageUrl)
+                .apply(
+                    RequestOptions()
+                        .placeholder(R.drawable.loading_animation)
+                        .error(R.drawable.ic_broken_image_gray_24dp))
+                .into(itemBinding.ivMangaCover)
         }
 
     }
