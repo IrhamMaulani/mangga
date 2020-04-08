79eeb97efe26d26e5b0e05537895292caa602dcf
Irham <irhammaulani@gmail.com>
2020-04-08 20:45:33 +0800

    change id to Long

--- app/src/main/java/com/example/mangga/model/Manga.kt
+++ app/src/main/java/com/example/mangga/model/Manga.kt
@@ -3,15 +3,18 @@ package com.example.mangga.model
 import com.squareup.moshi.Json
 
 data class Manga (
-    @Json(name = "mal_id") var id : String = "",
+    @Json(name = "mal_id") var id : Long = 0,
     @Json(name = "title") var title : String = "",
     @Json(name = "start_date") var startDate : String = "",
-    @field:Json(name = "end_date") var endDate : String = "",
+    @Json(name = "end_date") var endDate : String? = "",
     @Json(name = "score") var score : String = "",
-    @field:Json(name = "volumes") var volumes : String = "",
-    @Json(name = "image_url") var imageUrl : String = ""
+    @Json(name = "volumes") var volumes : String = "",
+    @Json(name = "image_url") var imageUrl : String = "",
+    @Json(name = "synopsis") var synopsis : String = "",
+    @Json(name = "publishing") var stillPublish : Boolean = false
 )
 
 data class MangaList(
     @Json(name = "results") var results: List<Manga>
 )
+
